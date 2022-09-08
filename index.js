22774b34132ec2428ca7ac9b8ffd999171b5e30d
Lina <linalitvinova92@gmail.com>
2022-09-08 11:27:07 +0200

    added function

--- index.js
+++ index.js
@@ -22,6 +22,30 @@ function isCTheLargest(a, b, c) {
   }
 }
 
+function isABEqual(a, b, c) {
+  if (a === b && a > c) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isBCEqual(a, b, c) {
+  if (b === c && b > a) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isACEqual(a, b, c) {
+  if (a === c && a > b) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 export function TheLargestNumber(a1, b1, c1) {
   let a = parseFloat(a1);
   let b = parseFloat(b1);
@@ -33,7 +57,17 @@ export function TheLargestNumber(a1, b1, c1) {
     return "The 2nd number is largest and equal: " + b;
   } else if (isCTheLargest(a, b, c)) {
     return "The 3rd number is largest and equal: " + c;
+  } else if (isABEqual(a, b, c)) {
+    return "The 1st and 2nd numbers are largest and equal: " + a;
+  } else if (isBCEqual(a, b, c)) {
+    return "The 2st and 3nd numbers are largest and equal: " + b;
+  } else if (isACEqual(a, b, c)) {
+    return "The 1st and 3nd numbers are largest and equal: " + c;
   } else {
     return "All three numbers are equal. And equal: " + a;
   }
 }
+
+
+const result = TheLargestNumber(5, 3, 5);
+console.log(result);
